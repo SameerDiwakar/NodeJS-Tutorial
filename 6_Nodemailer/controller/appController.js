692830955c356a7f98d2c83fe992a34307e8dcfe
Sameer Diwakar <diwakarsameer27@gmail.com>
2025-03-29 13:36:09 +0530

    Mail using Gmail(App Password)

--- 6_Nodemailer/controller/appController.js
+++ 6_Nodemailer/controller/appController.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const nodemailer = require("nodemailer");
+const Mailgen = require("mailgen");
 
 const signup = async (req, res) => {
     //Test account
@@ -37,7 +39,61 @@ const signup = async (req, res) => {
 };
 
 const getBill = (req, res) => {
-    res.status(201).json('Get Bill Successfull');
+  const {userEmail} = req.body
+    let config = {
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD  //Not able to get app password so placed a sample one
+        },
+    };
+    let transporter = nodemailer.createTransport(config);
+    let MailGenerator = new Mailgen({
+        theme: "default",
+        product: {
+            name: "Mailgen",
+            link: "https://mailgen.js/",
+        },
+    });
+    let response = {
+        body: {
+            name: "John Doe",
+            intro: "Your invoice is ready to be downloaded.",
+            table: {
+                data: [
+                    {
+                        item: "EmailJS",
+                        description: "EmailJS service",
+                        price: "$10.00",
+                    },
+                    {
+                        item: "Mailgen",
+                        description: "Mailgen service",
+                        price: "$20.00",
+                    },
+                ],
+            },
+            outro: "Looking forward to doing more business with you!",
+        },
+    };
+    let mail = MailGenerator.generate(response);
+
+    let message = {
+        from: process.env.EMAIL,
+        to: userEmail,
+        subject: "Invoice",
+        html: mail,
+    }
+
+    transporter.sendMail(message).then((info) => {
+        return res.status(201).json({
+            msg: 'Email sent successfully!',
+            info: info.messageId,
+            preview: nodemailer.getTestMessageUrl(info),
+        })
+    }).catch((error) => {
+        return res.status(500).json('Error sending email!'+ error)
+    })
 };
 
 module.exports = { signup, getBill };
